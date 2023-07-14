fba45f963abcf4e4cbda5b12e0e33176576c249a
YongJOoOo <we072200@naver.com>
2023-07-14 18:09:46 +0900

    230714 마뭄리

--- src/main/java/com/group/libraryapp/LibraryAppApplication.java
+++ src/main/java/com/group/libraryapp/LibraryAppApplication.java
@@ -2,6 +2,7 @@ package com.group.libraryapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class LibraryAppApplication {
