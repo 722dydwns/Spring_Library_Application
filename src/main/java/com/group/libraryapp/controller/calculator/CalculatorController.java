01c435bd26763f6880037265d4c3d042873a661f
YongJOoOo <we072200@naver.com>
2023-07-14 16:49:43 +0900

    수정

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController //컨트롤러로 등록시키는 어노테이션 *컨트롤러 : API 진입 지점이다.
 public class CalculatorController {
 
+
+
     @GetMapping("/add") //API 명세에 따라 Http 메소드는 GET, HTTP path = "/add" 인 API 등록
    public int addTwoNumbers(@RequestParam int num1, @RequestParam int num2){
        return num1 + num2;
    }
-
 
    @GetMapping("/add") //DTO 이것이 Data Transfer Object 이다.
     public int addTwoNum(CalculatorAddRequest request){
