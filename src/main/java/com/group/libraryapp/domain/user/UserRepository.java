db61571b5b52bf27de34b1c2ffd98516ed309613
YongJOoOo <we072200@naver.com>
2023-07-24 15:35:05 +0900

    JPA 사용하여 쿼리함수로 리팩토링

--- src/main/java/com/group/libraryapp/domain/user/UserRepository.java
+++ src/main/java/com/group/libraryapp/domain/user/UserRepository.java
@@ -2,10 +2,14 @@ package com.group.libraryapp.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     //JPA Repository를 상속하는 것만으로 자동 빈 등록된다.
     //즉 @Repository 안붙여도 인식한다
 
-    //여기서 name값 기준으로 User 얻도록 메소드 시그니처 만듬
-    User findByName(String name);
+    //여기서 name값 기준으로 User 얻도록 메소드 시그니처 만듬 - 반환 객체 없으면 null 체크하려고
+    Optional<User> findByName(String name);
+
 }
--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -13,23 +13,23 @@ import java.util.stream.Collectors;
 @Service
 public class UserServiceV2 { //JPA 사용 서비스
     //userRepository 인터페이스
-    private final UserRepository userReposiory;
+    private final UserRepository userRepository;
 
     //생성자로 주입받기
     public UserServiceV2(UserRepository userRepository){
-        this.userReposiory = userRepository;
+        this.userRepository = userRepository;
     }
 
     //1) 저장기능 리팩토링
     //저장 메소드
     public void saveUser(UserCreateRequest request) {
         //Entity 객체 생성하여 save처리
-        User u = userReposiory.save(new User(request.getName(), request.getAge()));
+        User u = userRepository.save(new User(request.getName(), request.getAge()));
         //u.getId() 할 수 있음
     }
     //2) 조회 기능 리팩토링
     public List<UserResponse> getUsers(){
-        return userReposiory.findAll().stream()
+        return userRepository.findAll().stream()
                 .map(UserResponse::new)
                 .collect(Collectors.toList());
     }
@@ -37,20 +37,19 @@ public class UserServiceV2 { //JPA 사용 서비스
     public void updateUsers(UserUpdateRequest request){
         //id값 기준으로 데이터 탐색한 뒤,
         //탐색한 결과, User가 존재하면 얻고, 없으면 예외 던짐
-        User user = userReposiory.findById(request.getId()) //id값을 기준으로 얻어옴
+        User user = userRepository.findById(request.getId()) //id값을 기준으로 얻어옴
                 .orElseThrow(IllegalArgumentException::new);
         //API통해 들어온 이름 값으로 업데이트
         user.updateName(request.getName());
         //갱신한 이름을 다시 저장
-        userReposiory.save(user);
+        userRepository.save(user);
     }
     //4) 삭제 기능 리팩토링
     public void deleteUsers(String name){ //이름 기준으로
-        User user = userReposiory.findByName(name);
-        if(user == null){ //널체크 후 예외 던지기
-            throw new IllegalArgumentException();
-        }
+        //널체크로
+        User user = userRepository.findByName(name).orElseThrow(IllegalArgumentException::new);
+
         //이제 얻어온 User를 DB 상에서 삭제처리
-        userReposiory.delete(user);
+        userRepository.delete(user);
     }
 }
