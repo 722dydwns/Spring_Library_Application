4211c6e480b3d9b8c2a46c94729580abeb5c95d8
YongJOoOo <we072200@naver.com>
2023-07-28 16:59:16 +0900

    JPA 연관관계 옵션을 사용하여 대출 기능와 반납 기능 리팩토링

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -19,7 +19,7 @@ public class User {
 
     //여러 개의 UserLoanHistory 갖고있고 싶음
     //주도권 없는 User 입장에서 mappedBy로 연관관게의 주도권 갖는 객체의 필드 이름을 명시해줘야 한다.
-    @OneToMany (mappedBy = "user") //즉, UserLoanHistory 의 user 필드를 주도궈 있는 애로 명시 - 이 값이 명시되어야 진정한 데이터 저장 가능
+    @OneToMany (mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true) //cascade 옵션 사용하여 연결 처리 함께해줌
     private List<UserLoanHistory> userLoanHistories = new ArrayList<>();
 
     //기본 생성자
@@ -51,4 +51,20 @@ public class User {
     public void updateName(String name){
         this.name = name;//변경하기
     }
+
+    //User를 통해 대출기록을 저장해보자.
+    public void loanBook(String bookName){
+        this.userLoanHistories.add(new UserLoanHistory(this, bookName));
+    }
+    //반납 기능 리팩토링
+    public void returnBook(String bookName){
+        //책 이름과 같은 대출 기록 객체를 얻어옴
+        UserLoanHistory targetHistory = this.userLoanHistories.stream()
+                .filter(history-> history.getBookName().equals(bookName))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+
+        targetHistory.doReturn();//해당 대출 기록에 대하여 doReturn 처리
+    }
+
 }
